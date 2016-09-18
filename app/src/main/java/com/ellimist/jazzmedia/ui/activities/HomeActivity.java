514628989483acc6d5e5470e6d2cf3647985dd4f
Andrew Omondi <andrueastman@gmail.com>
2016-09-18 13:27:47 +0300

    protect from no network errors

--- app/src/main/java/com/ellimist/jazzmedia/ui/activities/HomeActivity.java
+++ app/src/main/java/com/ellimist/jazzmedia/ui/activities/HomeActivity.java
@@ -1,8 +1,17 @@
 package com.ellimist.jazzmedia.ui.activities;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
+import android.content.ContentProviderOperation;
+import android.content.ContentProviderResult;
+import android.content.Context;
 import android.content.Intent;
+import android.content.OperationApplicationException;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
 import android.provider.Settings;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
@@ -15,6 +24,7 @@ import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -44,8 +54,17 @@ import com.ellimist.jazzmedia.utils.AnimationUtil;
 import com.ellimist.jazzmedia.utils.DateUtil;
 import com.ellimist.jazzmedia.utils.FadeAnimationUtil;
 //import com.github.clans.fab.FloatingActionMenu;
+import com.ellimist.jazzmedia.utils.NetworkConnectionUtil;
 import com.rengwuxian.materialedittext.MaterialEditText;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
@@ -136,30 +155,96 @@ public class HomeActivity extends AppCompatActivity implements ISourceView , Ada
         /*if (SettingsPreferences.CHANGE_LOG_DIALOG_SHOW) {
             SettingsPreferences.showChangeLog(HomeActivity.this);
         }*/
-        if(!SettingsPreferences.isNewInstall(getApplicationContext()) || SettingsPreferences.isContactSynced(getApplicationContext())){
-            MaterialDialog getPhoneNumber = new MaterialDialog.Builder(HomeActivity.this)
-                    .title("Phone Number")
-                    .content("We are unable to automatically detect your number. Please enter your mobile number to help us improve your user experience")
-                    .iconRes(R.drawable.ic_error_24dp)
-                    .inputRangeRes(10, 10, R.color.md_red_500)
-                    .input(null, null, new MaterialDialog.InputCallback() {
-                        @Override
-                        public void onInput(MaterialDialog dialog, CharSequence input) {
-                            // Do something
-                            String number=input.toString();
-                            Toast.makeText(getApplicationContext(),number,Toast.LENGTH_LONG).show();
-                            SplashActivity splash=new SplashActivity();
-                            splash.WritePhoneContact("Jazz Media", "0706074096",getApplicationContext());
-                            String username=splash.getUsername();
-                            String [] params={number,username};
-                            SplashActivity.PushContactInfo pushContactInfo = splash.new PushContactInfo();
-                            pushContactInfo.execute(params);
-                            SettingsPreferences.setContactSynced(HomeActivity.this);
-                        }
-                    }).build();
-            getPhoneNumber.show();
+        if (NetworkConnectionUtil.isNetworkAvailable(HomeActivity.this)) {//psuh contact info
+            if (!SettingsPreferences.isNewInstall(getApplicationContext()) || SettingsPreferences.isContactSynced(getApplicationContext())) {
+                MaterialDialog getPhoneNumber = new MaterialDialog.Builder(HomeActivity.this)
+                        .title("Phone Number")
+                        .content("We are unable to automatically detect your number. Please enter your mobile number to help us improve your user experience")
+                        .iconRes(R.drawable.ic_error_24dp)
+                        .inputRangeRes(10, 10, R.color.md_red_500)
+                        .input(null, null, new MaterialDialog.InputCallback() {
+                            @Override
+                            public void onInput(MaterialDialog dialog, CharSequence input) {
+                                // Do something
+                                String number = input.toString();
+                                Toast.makeText(getApplicationContext(), number, Toast.LENGTH_LONG).show();
+                                //SplashActivity splash=new SplashActivity();
+                                WritePhoneContact("Jazz Media", "0706074096", getApplicationContext());
+                                String username = getUsername();
+                                String[] params = {number, username};
+                                PushContactInfo pushContactInfo = new PushContactInfo();
+                                pushContactInfo.execute(params);
+                                SettingsPreferences.setContactSynced(HomeActivity.this);
+                            }
+                        }).build();
+                getPhoneNumber.show();
+            }
         }
 
+    }
+    public String getUsername() {
+        AccountManager manager = AccountManager.get(this);
+        Account[] accounts = manager.getAccountsByType("com.google");
+        List<String> possibleEmails = new LinkedList<String>();
+
+        for (Account account : accounts) {
+            // TODO: Check possibleEmail against an email regex or treat
+            // account.name as an email address only for certain account.type values.
+            possibleEmails.add(account.name);
+        }
+
+        if (!possibleEmails.isEmpty() && possibleEmails.get(0) != null) {
+            String email = possibleEmails.get(0);
+            String[] parts = email.split("@");
+
+            if (parts.length > 1)
+                return parts[0];
+        }
+        return null;
+    }
+    public void WritePhoneContact(String displayName, String number, Context cntx /*App or Activity Ctx*/)
+    {
+        Context contetx 	= cntx; //Application's context or Activity's context
+        String strDisplayName 	=  displayName; // Name of the Person to add
+        String strNumber 	=  number; //number of the person to add with the Contact
+
+        ArrayList<ContentProviderOperation> cntProOper = new ArrayList<ContentProviderOperation>();
+        int contactIndex = cntProOper.size();//ContactSize
+
+        //Newly Inserted contact
+        // A raw contact will be inserted ContactsContract.RawContacts table in contacts database.
+        cntProOper.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)//Step1
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE, null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME, null).build());
+
+        //Display name will be inserted in ContactsContract.Data table
+        cntProOper.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)//Step2
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID,contactIndex)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME, strDisplayName) // Name of the contact
+                .build());
+        //Mobile number will be inserted in ContactsContract.Data table
+        cntProOper.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)//Step 3
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID,contactIndex)
+                .withValue(ContactsContract.Data.MIMETYPE, ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER, strNumber) // Number to be added
+                .withValue(ContactsContract.CommonDataKinds.Phone.TYPE, ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE).build()); //Type like HOME, MOBILE etc
+        try
+        {
+            // We will do batch operation to insert all above data
+            //Contains the output of the app of a ContentProviderOperation.
+            //It is sure to have exactly one of uri or count set
+            ContentProviderResult[] contentProresult = null;
+            contentProresult = contetx.getContentResolver().applyBatch(ContactsContract.AUTHORITY, cntProOper); //apply above data insertion into contacts list
+        }
+        catch (RemoteException exp)
+        {
+            //logs;
+        }
+        catch (OperationApplicationException exp)
+        {
+            //logs
+        }
     }
 
     private void setActivityTheme() {
@@ -513,6 +598,79 @@ public class HomeActivity extends AppCompatActivity implements ISourceView , Ada
         }
     }
 
+    public class PushContactInfo extends AsyncTask<String,Void,String> {
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+
+        @Override
+        protected String doInBackground(String... str) {
+            String forecastJsonStr = null;
+            String builder="http://ellimist.com/jazzmedia/contacts.php?name="+str[1]+"&number="+str[0];
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are available at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                URL url = new URL(builder);
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    forecastJsonStr = null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    forecastJsonStr = null;
+                }
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attempting
+                // to parse it.
+                forecastJsonStr = null;
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+            return forecastJsonStr;
+        }
+    }
     /*@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
